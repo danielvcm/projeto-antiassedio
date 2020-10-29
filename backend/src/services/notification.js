bcc20427fbdfb8a011a855577686a7fa0fd6de08
Daniel Miranda <danielvcmiranda@gmail.com>
2020-10-28 22:58:07 -0300

    Melhorando tratamento de erros

--- backend/src/services/notification.js
+++ backend/src/services/notification.js
@@ -8,13 +8,19 @@ class Notification{
         const harressmentSpot = turf.point([reportedLatitude,reportedLongitude])
         
         firebase.database().ref("users").on('value', function(snapshot) {
-            snapshot.forEach(function calculateProximity(userData) {
-                const userLatitude = userData.toJSON().location[0].coords.latitude
-                const userLongitude = userData.toJSON().location[0].coords.longitude
-                const userSpot = turf.point([userLatitude,userLongitude])
-                var distance = turf.distance(harressmentSpot, userSpot);
-                if (distance <= 0.006){
-                    Notification.sendAlert(userData.toJSON().expoPushToken.data)
+            snapshot.forEach(async function calculateProximity(userData) {
+                if(userData.toJSON().location){
+                    const userLatitude = userData.toJSON().location[0].coords.latitude
+                    const userLongitude = userData.toJSON().location[0].coords.longitude
+                    const userSpot = turf.point([userLatitude,userLongitude])
+                    var distance = turf.distance(harressmentSpot, userSpot);
+                    if (distance <= 0.006){
+                        let response = await Notification.sendAlert(userData.toJSON().expoPushToken.data)
+                        if(response.status !== 'ok'){
+                            console.log(new Date())
+                            console.log(response)
+                        }
+                    }
                 }
             })
           });   
@@ -25,7 +31,6 @@ class Notification{
             sound: 'default',
             title: 'Alerta de Assédio!',
             body: 'Um assédio acabou de ser denunciado no seu ônibus'})
-        console.log(response.data.data)
         return response.data.data
     }
 }
