324cf6b34e7f0429451d1a312483186dc394bafe
Daniel Miranda <danielvcmiranda@gmail.com>
2020-10-28 22:57:36 -0300

    removendo imports não usados

--- backend/src/database/real-time-database.js
+++ backend/src/database/real-time-database.js
@@ -1,6 +1,4 @@
 const firebase = require('firebase')
-const turf = require('@turf/turf')
-const Notification = require('../services/notification')
 class RealTimeDatabase{
     static async saveOccurrence(location){
         firebase.database().ref("harrassementOccurrences").child(location.timestamp).update({
